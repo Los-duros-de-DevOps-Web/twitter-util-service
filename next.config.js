a206fbf70f0c7589e93258dd8625ecfd4448e489
GabrielSB19 <gasuarez2002@gmail.com>
2023-10-11 16:22:19 -0500

    setVariables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    URL_UTIL: "http://localhost:3000/",
+    URL_USER: "http://localhost:3001/",
+    URL_AUTH: "http://localhost:3002/",
+    URL_FRONT: "http://localhost:3003/",
+    URL_POSTNOTI: "http://localhost:3004/",
+  },
   async headers() {
     return [
       {
